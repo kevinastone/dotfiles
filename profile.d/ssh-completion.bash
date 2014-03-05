7591068587bcf5506ef466841e2dbd19b2395661
Kevin Stone <kevinastone@gmail.com>
2014-03-04 22:19:52 -0800

    Updated ssh completion to check for ssh config file first

--- profile.d/ssh-completion.bash
+++ profile.d/ssh-completion.bash
@@ -1,2 +1,4 @@
-
-complete -o default -o nospace -W "$(/usr/bin/env ruby -ne 'puts $_.split(/[,\s]+/)[1..-1].reject{|host| host.match(/\*|\?/)} if $_.match(/^\s*Host\s+/);' < $HOME/.ssh/config)" scp sftp ssh
+if [ -f $HOME/.ssh/config ]
+then
+	complete -o default -o nospace -W "$(/usr/bin/env ruby -ne 'puts $_.split(/[,\s]+/)[1..-1].reject{|host| host.match(/\*|\?/)} if $_.match(/^\s*Host\s+/);' < $HOME/.ssh/config)" scp sftp ssh
+fi
