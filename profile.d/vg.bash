dc23d7ea0b10974314afc1e72ca28c3348acd28a
Kevin Stone <kevinastone@gmail.com>
2014-04-08 14:10:12 -0700

    Added env variable so eventbrite vagrant uses shared virtualenvs
    Signed-off-by: Kevin Stone <kevinastone@gmail.com>

--- profile.d/vg.bash
+++ profile.d/vg.bash
@@ -1,16 +1,17 @@
 export EB_HOME="/Users/kevinstone/eventbrite/core"
 export EB_VM_MEMSIZE="4096"
+export EB_VG_NFS_VENV="yes"
 
 function vg() {
 
-   if [ $1 ]
-   then
-       pushd $EB_HOME/django/scripts/vagrant/vagrant_1.4/
-       vagrant $@
-       popd
-   else
-       cd $EB_HOME
-   fi
+    if [ $1 ]
+    then
+        pushd $EB_HOME/django/scripts/vagrant/vagrant_1.4/
+        vagrant $@
+        popd
+    else
+        cd $EB_HOME
+    fi
 }
 
 complete -F _vagrant vg
