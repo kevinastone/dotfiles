28e52e1f54afb8d2e592740a7b344b8cb8fc1045
Kevin Stone <kevinastone@gmail.com>
2014-05-02 12:50:24 -0700

    Updated vagrant alias

--- profile.d/vg.bash
+++ profile.d/vg.bash
@@ -1,13 +1,19 @@
 export EB_HOME="/Users/kevinstone/eventbrite/core"
 export EB_VM_MEMSIZE="4096"
-export EB_VG_NFS_VENV="yes"
+# export EB_VG_NFS_VENV="yes"
 export EB_VG_SYNC_VENV="yes"
+
+export VAGRANT_PATH="$EB_HOME/django/scripts/vagrant/vagrant_1.5"
+
+function __call_vagrant() {
+
+    VAGRANT_CWD="$VAGRANT_PATH" vagrant $@
+}
 
 function vg() {
 
     if [ $1 ]
     then
-        pushd $EB_HOME/django/scripts/vagrant/vagrant_1.4/
         case "$1" in
             up|resume|ssh|provision)
                 # Add the ssh-key on boot
@@ -15,15 +21,16 @@ function vg() {
             ;;
         	halt|suspend)
                 # Need to rsync back before shutdown
-    			vm_name=`vagrant status | grep running | head -1 | cut -f1 -d" "`
-                sync_cmd=`vagrant list-commands | grep rsync-back | head -1 | cut -f1 -d" "`
-    			if [[ "$vm_name" != "" && "$sync_cmd" != "" ]]; then
-    				vagrant $sync_cmd
-    			fi
+                if [ -n "$EB_VG_SYNC_VENV" ]; then
+        			vm_name=`vagrant status | grep running | head -1 | cut -f1 -d" "`
+                    sync_cmd=`vagrant list-commands | grep rsync-back | head -1 | cut -f1 -d" "`
+        			if [[ "$vm_name" != "" && "$sync_cmd" != "" ]]; then
+                        __call_vagrant $sync_cmd
+        			fi
+                fi
 			;;
     	esac
-        vagrant $@
-        popd
+        __call_vagrant $@
     else
         cd $EB_HOME
     fi
