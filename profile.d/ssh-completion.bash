98c50ed51c4b6fe2937da065f3c06d58ded86257
Kevin Stone <kevinastone@gmail.com>
2014-07-27 15:44:41 -0700

    Updated bash completion

--- profile.d/ssh-completion.bash
+++
@@ -1,4 +0,0 @@
-if [ -f $HOME/.ssh/config ]
-then
-	complete -o default -o nospace -W "$(/usr/bin/env ruby -ne 'puts $_.split(/[,\s]+/)[1..-1].reject{|host| host.match(/\*|\?/)} if $_.match(/^\s*Host\s+/);' < $HOME/.ssh/config)" scp sftp ssh
-fi
