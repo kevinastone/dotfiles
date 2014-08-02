a8120a9ded6b530d20fdd975ad1064e883a91b21
Kevin Stone <kevinastone@gmail.com>
2014-08-02 16:59:23 -0700

    Added hub fish completion

---
+++ config/fish/completions/hub.fish
@@ -0,0 +1,3 @@
+complete -c hub -xa 'alias pull-request fork create browse compare ci-status'
+
+complete -c hub -fa pull-request
--- config/fish/completions/workon.fish
+++ config/fish/completions/workon.fish
@@ -1,2 +1,2 @@
 
-complete -c workon -fAa '(for d in $WORKON_BASE_DIR/*; test -d "$d"; and echo (basename $d); end)'
+complete -c workon -xa '(for d in $WORKON_BASE_DIR/*; test -d "$d"; and echo (basename $d); end)'
