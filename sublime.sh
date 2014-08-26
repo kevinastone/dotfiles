3c987c17e4a7aa4eeb28dc4e56e4683e7e8d033e
Kevin Stone <kevinastone@gmail.com>
2014-08-26 10:06:25 -0700

    Added linking dropbox folder for sublime

--- sublime.sh
+++ sublime.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
-mkdir -p ~/Library/"Application Support"/"Sublime Text 3"/"Installed Packages"
-cd ~/Library/"Application Support"/"Sublime Text 3"/"Installed Packages"
-[ -e "Package Control.sublime-package" ] || wget https://sublime.wbond.net/Package%20Control.sublime-package
+mkdir -p $HOME/Library/"Application Support"/"Sublime Text 3"/"Installed Packages"
+cd $HOME/Library/"Application Support"/"Sublime Text 3"/"Installed Packages"
+[ -e "Package Control.sublime-package" ] || {
+    wget https://sublime.wbond.net/Package%20Control.sublime-package
+}
+
+if [ -e $HOME/Dropbox/AppData/Sublime/User ]; then
+
+    [ "$(readlink $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User)" = "$HOME/Dropbox/AppData/Sublime/User" ] || {
+        mv $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/orig.User
+        ln -sf $HOME/Dropbox/AppData/Sublime/User $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User
+    }
+else
+    echo "No Dropbox User Folder"
+fi
