c9f79d4d9d4f8243ce3081ab737d50dbf0086ae1
Kevin Stone <kevinastone@gmail.com>
2014-10-11 10:40:21 -0700

    Added pullall helper

---
+++ config/fish/completions/pullall.fish
@@ -0,0 +1,1 @@
+complete -x -c pullall -a "(__fish_complete_cd)"
---
+++ config/fish/functions/pullall.fish
@@ -0,0 +1,15 @@
+
+function pullall --description "Git Pull all Repos under the current path"
+    set -q argv[1]
+    and set -l path $argv[1]
+    or set -l path $PWD
+    
+    for git_dir in (find "$path" -name .git -type d)
+        pushd $git_dir/..
+        set_color blue
+        echo $PWD
+        set_color normal
+        git pull --rebase -p
+        popd
+    end
+end
