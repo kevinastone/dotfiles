a18288c1b9a2adb9d919796e7f3837d4b4ceb91a
Kevin Stone <kevinastone@gmail.com>
2016-07-09 09:00:26 -0700

    Added vendor completion path for fish on osx

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,6 +6,11 @@ end
 
 ulimit -n 4096
 
+# Fish Completions
+if test -d /usr/local/share/fish/vendor_completions.d
+    set -x fish_complete_path $fish_complete_path /usr/local/share/fish/vendor_completions.d
+end
+
 # Config.d
 for conf in $HOME/.config/fish/config.d/**.fish
     source $conf
