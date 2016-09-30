ee8fe064a5d6ec4eac8c2a62c13b14dd70d048d7
Kevin Stone <kevinastone@gmail.com>
2016-09-30 08:14:36 -0700

    Check path exists before adding

--- config/fish/config.d/node.fish
+++ config/fish/config.d/node.fish
@@ -1,3 +1,3 @@
-if status --is-login
+if status --is-login; and test -d ./node_modules/.bin
     set -x PATH ./node_modules/.bin $PATH
 end
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,11 @@
 set -x EDITOR vim
 
 if status --is-login
-    set -x PATH ~/.bin ~/.local/bin $PATH
+    set -x PATH ~/.bin $PATH
+end
+
+if status --is-login; and test -d ~/.local/bin
+    set -x PATH ~/.local/bin $PATH
 end
 
 ulimit -n 4096
