65465a7785ca756be907bd4c9363575da6bf8938
Kevin Stone <kevinastone@gmail.com>
2017-04-22 18:19:43 -0700

    Moved to conf.d dir for fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,8 +14,3 @@ ulimit -n 4096
 if test -d /usr/local/share/fish/vendor_completions.d
     set -x fish_complete_path $fish_complete_path /usr/local/share/fish/vendor_completions.d
 end
-
-# Config.d
-for conf in $HOME/.config/fish/config.d/**.fish
-    source $conf
-end
