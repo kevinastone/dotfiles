c0182164f6f31d7b5abba83ac40d410377ffea44
Kevin Stone <kevinastone@gmail.com>
2018-05-04 20:21:59 -0700

    Added vlcrc

---
+++ macos.d/vlc.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# VLC
+###############################################################################
+
+mkdir -p ~/Library/Preferences/org.videolan.vlc
+cp vlcrc ~/Library/Preferences/org.videolan.vlc/
