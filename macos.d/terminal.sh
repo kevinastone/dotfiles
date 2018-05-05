75887cae97c50792d099719bcfba4e1955686c67
Kevin Stone <kevinastone@gmail.com>
2018-05-05 15:53:24 -0700

    Updated terminal setup

--- macos.d/terminal.sh
+++ macos.d/terminal.sh
@@ -8,7 +8,8 @@ defaults write com.apple.Terminal NewTabWorkingDirectoryBehavior -bool true
 
 pushd "${BASH_SOURCE%/*}" &>/dev/null
 /usr/libexec/PlistBuddy \
-    -c 'Set :"Window Settings":Basic:shellExitAction bool true' \
+    -c 'Delete :"Window Settings":Basic:shellExitAction' \
+    -c 'Add :"Window Settings":Basic:shellExitAction bool true' \
     -c 'Delete :"Window Settings":Basic:noWarnProcesses' \
     -c 'Add :"Window Settings":Basic:noWarnProcesses array' \
     -c 'Merge nowarn.terminal.plist :"Window Settings":Basic:noWarnProcesses' \
