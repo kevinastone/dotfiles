22b897e4cb578ab0d0326da24fe7f1bb5cd3b8e8
Kevin Stone <kevinastone@gmail.com>
2018-05-30 20:39:37 -0700

    updated control strip

--- macos.d/input.sh
+++ macos.d/input.sh
@@ -53,7 +53,7 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightC
 # com.apple.system.sleep
 # com.apple.system.volume
 
-defaults write com.apple.controlstrip FullCustomized -array 'com.apple.system.group.brightness' 'com.apple.system.mission-control' 'com.apple.system.launchpad' 'com.apple.system.group.keyboard-brightness' 'com.apple.system.group.media' 'com.apple.system.group.volume' 'com.apple.system.screen-lock'
+defaults write com.apple.controlstrip FullCustomized -array 'com.apple.system.group.brightness' 'com.apple.system.mission-control' 'com.apple.system.show-desktop' 'com.apple.system.group.keyboard-brightness' 'com.apple.system.group.media' 'com.apple.system.group.volume' 'com.apple.system.screen-lock'
 defaults write com.apple.controlstrip MiniCustomized -array 'com.apple.system.media-play-pause' 'com.apple.system.volume' 'com.apple.system.mute' 'com.apple.system.screen-lock'
 
 # Prefer full control-strip
