62b56a4c69b6112a9ff54a67e1a2b61aa04f401d
Kevin Stone <kevinastone@gmail.com>
2018-08-05 14:52:41 -0700

    Added more android configuration

--- config/fish/conf.d/android.fish
+++ config/fish/conf.d/android.fish
@@ -1,3 +1,12 @@
-if test -d /opt/android_sdk/platform-tools
-    set -x PATH /opt/android_sdk/platform-tools $PATH
+if test -d /opt/android_sdk
+
+    # exports
+    set -x ANDROID_SDK "/opt/android_sdk"
+    set -x ANDROID_HOME "$ANDROID_SDK"
+
+    for dir  in tools platform-tools
+        if test -d "$ANDROID_SDK"/"$dir"
+            set -x PATH "$ANDROID_SDK"/"$dir" $PATH
+        end
+    end
 end
