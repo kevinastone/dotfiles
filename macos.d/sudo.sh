3a2c039f30fd805941964f602171a00eaa995fde
Kevin Stone <kevinastone@gmail.com>
2018-11-13 22:08:09 -0800

    Added htop sudoers

--- macos.d/sudo.sh
+++ macos.d/sudo.sh
@@ -8,4 +8,5 @@ pushd "${BASH_SOURCE%/*}" &>/dev/null
     diff -u /etc/pam.d/sudo sudo || {
         (umask 0333 && sudo cp -i sudo /etc/pam.d/sudo)
     }
+    sudo cp htop /etc/sudoers.d/
 popd &>/dev/null
