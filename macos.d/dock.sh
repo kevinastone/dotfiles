d671d2681c39400375056c4b10675c8d25ed9790
Kevin Stone <kevinastone@gmail.com>
2019-08-02 10:09:49 -0500

    Set dock tile height

--- macos.d/dock.sh
+++ macos.d/dock.sh
@@ -5,5 +5,7 @@
 ###############################################################################
 
 defaults write com.apple.dock show-recents -bool false
+# Set the tile size (and hence dock height) to 64
+defaults write com.apple.dock tilesize -int 64
 
 killall Dock &> /dev/null
