f0a8680f7b9d225ecde9472d7c47037137fce208
Kevin Stone <kevinastone@gmail.com>
2020-06-29 00:17:31 -0500

    Added fisher to manage fish shell plugins

--- config/fish/conf.d/base16.fish
+++ config/fish/conf.d/base16.fish
@@ -1,5 +1,3 @@
-# Base16 Shell
-if status --is-interactive; and test -d "$HOME/.config/base16-shell"
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
+if test -n "$base16_theme"
+  base16-$base16_theme
 end
