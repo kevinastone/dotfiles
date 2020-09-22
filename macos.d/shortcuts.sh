5b6abb8802891ff4767633deb811f62ab6332860
Kevin Stone <kevinastone@gmail.com>
2020-09-22 10:01:55 -0500

    Added macos keyboard shortcuts

---
+++ macos.d/shortcuts.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2016
+
+###############################################################################
+# Keyboard Shortcuts                                                          #
+###############################################################################
+
+# command = $
+# control = ^
+# option = ~
+# shift = @
+
+defaults write NSGlobalDomain NSUserKeyEquivalents -dict-add "Emoji & Symbols" '@$e'
