5f2c3cb28f00e10cf1e16252a4cdc4e608a067df
Kevin Stone <kevinastone@gmail.com>
2022-11-23 17:27:38 -0800

    Fix time machine config exclusions

--- macos.d/time_machine.sh
+++ macos.d/time_machine.sh
@@ -5,5 +5,5 @@
 ###############################################################################
 
 for item in "~kstone/Downloads" "~kstone/Dropbox" "~kstone/Documents/Virtualbox VMs" "~kstone/.local" "~kstone/Movies" "~kstone/Applications"; do
-    tmutil isexcluded "$item" || tmutil addexclusion -p "$item"
+    tmutil isexcluded "$item" | grep '[Excluded]' || sudo tmutil addexclusion -p "$item"
 done
