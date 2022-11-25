c3245c0e3c43c4a0bc4ecfc6e4024854139f14b8
Kevin Stone <kevinastone@gmail.com>
2022-11-24 22:25:26 -0800

    Updated macos terminal config

--- macos.d/terminal.sh
+++ macos.d/terminal.sh
@@ -13,5 +13,6 @@ pushd "${BASH_SOURCE%/*}" &>/dev/null
     -c 'Add :"Window Settings":Basic:noWarnProcesses array' \
     -c 'Merge nowarn.terminal.plist :"Window Settings":Basic:noWarnProcesses' \
     -c 'Set :"Window Settings":Basic:useOptionAsMetaKey true' \
+    -c 'Set :"Window Settings":Basic:shellExitAction 1' \
     ~/Library/Preferences/com.apple.Terminal.plist
 popd &>/dev/null
