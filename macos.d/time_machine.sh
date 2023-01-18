446400f78c9d24664db89d2c8c58a37ceb29cffb
Kevin Stone <kevinastone@gmail.com>
2023-01-17 21:18:44 -0800

    Migrate to new Dropbox location

--- macos.d/time_machine.sh
+++ macos.d/time_machine.sh
@@ -4,6 +4,6 @@
 # Time Machine
 ###############################################################################
 
-for item in "~kstone/Downloads" "~kstone/Dropbox" "~kstone/Documents/Virtualbox VMs" "~kstone/.local" "~kstone/Movies" "~kstone/Applications"; do
+for item in "~kstone/Downloads" "~kstone/Library/CloudStorage/Dropbox" "~kstone/Documents/Virtualbox VMs" "~kstone/.local" "~kstone/Movies" "~kstone/Applications"; do
     tmutil isexcluded "$item" | grep '[Excluded]' || sudo tmutil addexclusion -p "$item"
 done
--- sublime.sh
+++ sublime.sh
@@ -5,17 +5,17 @@ cd $HOME/Library/"Application Support"/"Sublime Text 3"/"Installed Packages"
     wget https://packagecontrol.io/Package%20Control.sublime-package
 }
 
-if [ -e $HOME/Dropbox/AppData/Sublime/User ]; then
+if [ -e $HOME/Library/CloudStorage/Dropbox/AppData/Sublime/User ]; then
 
     if [ -e $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User ]; then
-        [ "$(readlink $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User)" = "$HOME/Dropbox/AppData/Sublime/User" ] || {
+        [ "$(readlink $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User)" = "$HOME/Library/CloudStorage/Dropbox/AppData/Sublime/User" ] || {
             mv $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/orig.User
-            ln -sf $HOME/Dropbox/AppData/Sublime/User $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User
+            ln -sf $HOME/Library/CloudStorage/Dropbox/AppData/Sublime/User $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User
         }
     else
         mkdir -p $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"
-        [ "$(readlink $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User)" = "$HOME/Dropbox/AppData/Sublime/User" ] || {
-            ln -sf $HOME/Dropbox/AppData/Sublime/User $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User
+        [ "$(readlink $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User)" = "$HOME/Library/CloudStorage/Dropbox/AppData/Sublime/User" ] || {
+            ln -sf $HOME/Library/CloudStorage/Dropbox/AppData/Sublime/User $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User
         }
     fi
 else
