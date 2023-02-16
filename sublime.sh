daf82238db59dfd9fd7e6407b84eb3cce0d3ed7f
Kevin Stone <kevinastone@gmail.com>
2023-02-16 13:05:28 -0800

    Remove installing sublime package manager
    It's installed by default now

--- sublime.sh
+++ sublime.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-mkdir -p $HOME/Library/"Application Support"/"Sublime Text 3"/"Installed Packages"
-cd $HOME/Library/"Application Support"/"Sublime Text 3"/"Installed Packages"
-[ -e "Package Control.sublime-package" ] || {
-    wget https://packagecontrol.io/Package%20Control.sublime-package
-}
-
 if [ -e $HOME/Library/CloudStorage/Dropbox/AppData/Sublime/User ]; then
 
     if [ -e $HOME/Library/"Application Support"/"Sublime Text 3"/"Packages"/User ]; then
