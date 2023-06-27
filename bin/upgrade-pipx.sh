8c164b559dab8260669202c363d461f29bdc2f48
Kevin Stone <kevinastone@gmail.com>
2023-06-26 18:18:54 -0700

    Fix upgrade-pipx to discover path to system python3

--- bin/upgrade-pipx.sh
+++ bin/upgrade-pipx.sh
@@ -6,7 +6,7 @@ cd ~/.local/pipx/venvs || exit
 PY_VERSION="$1"
 shift
 
-PYTHON_BIN=$(realpath /usr/local/bin/python3)
+PYTHON_BIN=$(realpath "$(which python3)")
 
 exes=()
 while IFS= read -r line; do exes+=("$line"); done < <(find . -path "./*/bin/${PY_VERSION}" -print)
