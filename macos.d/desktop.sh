5b5cf8297aec19d0a5bc6b81ccab1f940f831235
Kevin Stone <kevinastone@gmail.com>
2023-12-17 12:53:33 -0800

    Capture desktop preferences for macos

---
+++ macos.d/desktop.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Desktop                                                                     #
+###############################################################################
+
+defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool false
+defaults write com.apple.WindowManager HasDisplayedShowDesktopEducation -bool true
