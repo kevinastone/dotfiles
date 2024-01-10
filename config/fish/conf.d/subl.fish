df143a7fe718bbf45f3ca969386a04bef09ddf86
Kevin Stone <kevinastone@gmail.com>
2024-01-10 12:09:52 -0800

    Remove unnecessary subl fish config

--- config/fish/conf.d/subl.fish
+++
@@ -1,5 +0,0 @@
-if status --is-login; and test -d "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
-    contains "/Applications/Sublime Text.app/Contents/SharedSupport/bin" $fish_user_paths
-    or set -x fish_user_paths "/Applications/Sublime Text.app/Contents/SharedSupport/bin" $fish_user_paths
-end
-
