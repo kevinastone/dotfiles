9728bfdd2d281b5ddda66dca30c875e6839d3446
Kevin Stone <kevinastone@gmail.com>
2025-11-30 16:11:25 -0800

    Add casks and MAS apps

--- flake.nix
+++ flake.nix
@@ -58,8 +58,23 @@
         # onActivation.cleanup = "uninstall";
 
         taps = [];
-        brews = [ "cowsay" ];
-        casks = [];
+        brews = [];
+        casks = [
+          "1password"
+          "alfred"
+          "ghostty"
+          "hammerspoon"
+          "keepingyouawake"
+          "spotify"
+          "sublime-text"
+          "the-unarchiver"
+          "vlc"
+        ];
+        masApps = {
+          "PDF Expert" = 1055273043;
+          "Pixelmator Pro" = 1289583905;
+          "Affinity Designer" = 824171161;
+        };
       };
     };
 
