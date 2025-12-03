61f770229b105ab974c0cdf231ef25862c642eeb
Kevin Stone <kevinastone@gmail.com>
2025-12-02 20:02:17 -0800

    Removed cruft in sudo darwin helper

--- nix/darwin/sudo.nix
+++ nix/darwin/sudo.nix
@@ -2,17 +2,9 @@
   config,
   lib,
   username,
-  pkgs,
   ...
 }:
 let
-  # profileDirectory = config.home-manager.users."${username}".home.profileDirectory;
-  # resolveExecutableBasename = item:
-  #   if lib.isString item then
-  #     item
-  #   else
-  #     item.meta.mainProgram;
-  # executables = map resolveExecutableBasename config.home-manager.users."${username}".sudo.nopasswd-executables;
   paths = config.home-manager.users."${username}".sudo.nopasswd.paths;
 in
 lib.mkIf (paths != [ ]) {
