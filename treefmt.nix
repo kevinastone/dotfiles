a35e5c8ae3ad17839b2ff87dade8e8f51a787647
Kevin Stone <kevinastone@gmail.com>
2025-12-03 18:46:48 -0800

    Added prettier formatter for json + yaml

--- treefmt.nix
+++ treefmt.nix
@@ -4,8 +4,10 @@ _: {
   programs.fish_indent.enable = true;
   # programs.shellcheck.enable = true;
 
-  # programs.jsonfmt.enable = true;
-  programs.yamlfmt.enable = true;
+  # JSON/YAML formatter
+  programs.prettier.enable = true;
+  settings.formatter.prettier.includes = [ "*.{json,yaml}" ];
+
   # toml formatter
   programs.taplo.enable = true;
 
@@ -13,10 +15,10 @@ _: {
 
   programs.keep-sorted.enable = true;
 
+  # Nix formatters
   programs.nixfmt.enable = true;
   programs.statix.enable = true;
   programs.deadnix.enable = true;
-
   settings.formatter = {
     deadnix.priority = 1;
     statix.priority = 2;
