cc45f044d52a69a86e8bf7369190276debe8ebfb
Kevin Stone <kevinastone@gmail.com>
2025-12-08 14:52:03 -0800

    Seperated graphic design packages into their own homebrew module

--- nix/darwin/homebrew/default.nix
+++ nix/darwin/homebrew/default.nix
@@ -24,10 +24,5 @@
     };
     taps = [ ];
     brews = [ "mas" ];
-    masApps = {
-      "PDF Expert" = 1055273043;
-      "Pixelmator Pro" = 1289583905;
-      "Affinity Designer" = 824171161;
-    };
   };
 }
---
+++ nix/darwin/homebrew/graphics/default.nix
@@ -0,0 +1,7 @@
+_: {
+  homebrew.masApps = {
+    "PDF Expert" = 1055273043;
+    "Pixelmator Pro" = 1289583905;
+    "Affinity Designer" = 824171161;
+  };
+}
