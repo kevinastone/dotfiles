f44dceb429427b10916b77a9f5f18d614d08e1db
Kevin Stone <kevinastone@gmail.com>
2025-12-30 15:31:50 -0800

    Update k9s cnpg plugin commands

--- nix/home/admin/k8s/k9s/cnpg.nix
+++ nix/home/admin/k8s/k9s/cnpg.nix
@@ -1,15 +1,48 @@
-_: {
-  programs.k9s.plugins.cnpg-logs = {
-    shortCut = "l";
-    description = "Logs";
-    scopes = [
-      "cluster"
-    ];
-    command = "bash";
-    background = false;
-    args = [
-      "-c"
-      "kubectl cnpg logs cluster $NAME -f -n $NAMESPACE --context $CONTEXT"
-    ];
+_:
+let
+  argsPrefix = "kubectl cnpg --context $CONTEXT -n $NAMESPACE";
+  mkCnpgPlugin =
+    {
+      command,
+      shortCut,
+      scopes ? [ "cluster" ],
+      ...
+    }@params:
+    {
+      inherit shortCut;
+      description = "cnpng ${command}";
+      inherit scopes;
+      command = "bash";
+      background = false;
+      args = [
+        "-c"
+        "${argsPrefix} ${command} $NAME | less"
+      ];
+    }
+    // (builtins.removeAttrs params [
+      "command"
+      "shortCut"
+      "scopes"
+    ]);
+in
+{
+  programs.k9s.plugins = {
+    cnpg-logs = {
+      shortCut = "l";
+      description = "Logs";
+      scopes = [
+        "cluster"
+      ];
+      command = "bash";
+      background = false;
+      args = [
+        "-c"
+        "kubectl cnpg logs cluster $NAME -f -n $NAMESPACE --context $CONTEXT"
+      ];
+    };
+    cnpg-status = mkCnpgPlugin {
+      shortCut = "Shift-S";
+      command = "status";
+    };
   };
 }
