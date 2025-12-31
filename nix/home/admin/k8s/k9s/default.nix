eb98bae9c2b6fa370ec2e081577b2b76530ae2de
Kevin Stone <kevinastone@gmail.com>
2025-12-30 18:59:18 -0800

    Fix k9s dialog contrast

--- nix/home/admin/k8s/k9s/default.nix
+++ nix/home/admin/k8s/k9s/default.nix
@@ -1,4 +1,7 @@
-{ ... }:
+{ lib, pkgs, ... }:
+let
+  preferencesPath = if pkgs.stdenv.isDarwin then "Library/Application Support/" else ".config";
+in
 {
   imports = [
     ./cnpg.nix
@@ -6,7 +9,10 @@
   ];
 
   programs.k9s.enable = true;
-  programs.k9s.settings.k9s = {
-    ui.logoless = true;
-  };
+  programs.k9s.settings.k9s.ui.logoless = true;
+
+  # Workaround for catppuccin/k9s defects
+  # https://github.com/catppuccin/k9s/issues/16
+  home.file."${preferencesPath}/k9s/skins/catppuccin-mocha.yaml".source =
+    lib.mkForce ./catppuccin-mocha.yaml;
 }
