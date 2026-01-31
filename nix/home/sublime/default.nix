a7ad263caf0908f0fcc8dd0246db3423b283ba9d
Kevin Stone <kevinastone@gmail.com>
2026-01-31 12:36:04 -0800

    Add Sublime-text preferences file

--- nix/home/sublime/default.nix
+++ nix/home/sublime/default.nix
@@ -1,7 +1,15 @@
 { lib, pkgs, ... }:
+let
+  preferencesPath =
+    if pkgs.stdenv.isDarwin then "Library/Application Support/Sublime Text" else ".config/sublime-text";
+in
 {
   # Only linux can be installed via nixpkgs
   home.packages = lib.optionals pkgs.stdenv.isLinux [ pkgs.sublime4 ];
   # Use homebrew for MacOS
   homebrew.casks = lib.optionals pkgs.stdenv.isDarwin [ "sublime-text" ];
+
+  # Rely on sublime to merge configuration from other Packages
+  home.file."${preferencesPath}/Packages/Declarative/Preferences.sublime-settings".source =
+    ./Preferences.sublime-settings;
 }
