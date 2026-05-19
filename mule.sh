f9476bf973c89553cd62897c9497bd64223b7459
Kevin Stone <kevinastone@gmail.com>
2026-05-19 10:15:57 -0700

    Add mule script to setup mule installed packages

---
+++ mule.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mule install -upgrade \
+    duti \
+    fish-shell \
+    ghostty \
+    git \
+    gmosh \
+    jetski \
+    roadwarrior \
+    snipit \
+    spotify \
+    sublime-text \
+    tmux \
+    vlc
