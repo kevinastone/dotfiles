682df2334a1f0edaa63df126a3f96766710943bc
Kevin Stone <kevinastone@gmail.com>
2026-09-10 07:28:01 -0700

    fix(nix): correct catppuccin cachix trusted public key

--- nix/darwin/default.nix
+++ nix/darwin/default.nix
@@ -21,7 +21,7 @@
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "catppuccin.cachix.org-1:noG/4HkbhJb+lUJaKrQ3aoNz51unPVq54DuJZLLgkCg="
+      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
     ];
   };
 
